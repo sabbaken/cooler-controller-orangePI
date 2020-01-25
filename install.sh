bf2f93cef4d281b95020c74d902caa08564a88e2
Sabbaken <kirillverenih@ya.ru>
2020-01-25 22:06:21 +0300

    Add installer

---
+++ install.sh
@@ -0,0 +1,28 @@
+#Installing dependency
+git clone https://github.com/zhaolei/WiringOP.git -b h3 
+cd WiringOP
+chmod +x ./build
+sudo ./build
+cd ..
+rm -R WiringOP
+
+
+#move everything to my_scrypts folder
+mkdir -p $HOME/my_scripts/
+mkdir -p $HOME/my_scripts/cooler_controller/
+cp cooler_controller.sh $HOME/my_scripts/cooler_controller/
+
+#make script executable
+sudo chmod +x $HOME/my_scripts/cooler_controller/cooler_controller.sh
+
+#add service
+sudo cp cooler_controller.service /etc/systemd/system/
+sudo systemctl enable cooler_controller.service
+
+#delete files
+cd ..
+rm -R cooler_controller_for_orangePI
+
+echo [DONE]
+echo "Use [sudo systemctl status cooler_controller.service] to check service status"
+echo "Script has been installed in " $HOME/my_scripts/cooler_controller
