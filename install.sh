a0bef4c0e2837b979e6f086eb51da1f810ceee5b
Sabbaken <kirillverenih@ya.ru>
2020-01-25 22:36:35 +0300

    Add service enabling

--- install.sh
+++ install.sh
@@ -6,28 +6,33 @@ sudo ./build
 cd ..
 rm -R WiringOP
 
-
-#move everything to my_scrypts folder
+#Move everything to my_scrypts folder
 mkdir -p $HOME/my_scripts/
 mkdir -p $HOME/my_scripts/cooler_controller/
 cp cooler_controller.sh $HOME/my_scripts/cooler_controller/
 
-#make script executable
+#Make script executable
 sudo chmod +x $HOME/my_scripts/cooler_controller/cooler_controller.sh
 
-#add service
+#Add home path to service
 echo "ExecStart="$HOME"/my_scripts/cooler_controller/cooler_controller.sh" >> cooler_controller.service
 echo "
 [Install]
 WantedBy=multi-user.target" >> cooler_controller.service
 
+#Add service
 sudo cp cooler_controller.service /etc/systemd/system/
 sudo systemctl enable cooler_controller.service
 
-#delete files
+#Delete files
 cd ..
 rm -R cooler-controller-orangePI/
 
-echo [DONE]
-echo "Use [sudo systemctl status cooler_controller.service] to check service status"
-echo "Script has been installed in " $HOME/my_scripts/cooler_controller
+#Enable service
+sudo systemctl enable cooler_controller.service
+
+echo "==============================[ DONE ]=============================="
+echo "To check service status use"
+echo "	sudo systemctl status cooler_controller.service"
+echo "Script has been installed in "
+echo "	cd $HOME/my_scripts/cooler_controller"
